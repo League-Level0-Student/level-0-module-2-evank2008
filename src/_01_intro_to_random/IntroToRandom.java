e464fd298637d63ea7c53e25625b7891b65c7438
root <root@6df4ce45a0e2>
2020-03-26 18:26:12 +0000

    diceroll

--- src/_01_intro_to_random/IntroToRandom.java
+++ src/_01_intro_to_random/IntroToRandom.java
@@ -12,12 +12,11 @@ public class IntroToRandom {
 		
 		//3   Now to make num random. 
 		//3a. Create an object of the Random class, Hint: Random ran = new Random()
+		Random random = new Random();
 		
-		//3b. Next, set the value of num, using .nextInt() to get a random number from the Random object
-		
-
+		num=random.nextInt(-222 - 76);
 		//4. Now limit the value of num to be between 0 and 100
-
+System.out.println(num);
 		//5. Now limit the value of num to be between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
 
 		//6. Challenge: Limit the value of num to be between -222 and 88
